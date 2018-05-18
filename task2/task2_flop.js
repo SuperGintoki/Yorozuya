93aea1f72be4c8e49f3a5f85232b938dc828112a
SuperGintoki <helloxyy@outlook.com>
2018-05-18 15:06:41 +0800

    delete

--- task2/task2_flop.js
+++
@@ -1,6 +0,0 @@
-//获取左上角返回键图标
-var goBack = document.getElementsByTagName("header")[0].getElementsByTagName("i")[0];
-
-goBack.onclick=function(){
-	window.location.href = "task2_assignment.html";
-}
